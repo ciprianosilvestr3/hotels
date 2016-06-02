23d74ae23b0c9bfdd7fa351147dd91f619f11831
ciprianosilvestr3 <cipriano.silvestre@creandodm.com>
2016-06-02 11:15:27 -0500

    .2.3

--- application/controllers/hoteles.php
+++ application/controllers/hoteles.php
@@ -27,14 +27,7 @@ class Hoteles extends CI_Controller {
 	public function index()
 	{
 		$session_data = $this->session->userdata('creand6_token');
-		
-		$parametros=array(
-			'idusuario' =>$session_data['id'],
-			'usuario'=>$session_data['usuario'],
-			'tipo'=>$session_data['perfil'],
-			'nombre'=>$session_data['nombre']	
-		);
-		$this->layout->view('hoteles',$parametros);	
+		$this->layout->view('hoteles',$session_data);	
 	}
 
 }
