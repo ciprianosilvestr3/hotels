d15ec0cf453841cf3fe6512e29ade98ded019e8e
ciprianosilvestr3 <cipriano.silvestre@creandodm.com>
2016-06-02 10:46:54 -0500

    update

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -42,6 +42,53 @@ class Inicio extends CI_Controller {
 	$this->layout->view('inicio');	
 	}
 	
+	public function menu()
+	{
+
+		$response = array();
+		$response['menu']='<!-- DOC: To remove the sidebar toggler from the sidebar you just need to completely remove the below "sidebar-toggler-wrapper" LI element -->
+                        <li class="sidebar-toggler-wrapper hide">
+                            <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+                            <div class="sidebar-toggler"> </div>
+                            <!-- END SIDEBAR TOGGLER BUTTON -->
+                        </li>';
+                        /*<li class="sidebar-search-wrapper">
+                            <form class="sidebar-search  " action="page_general_search_3.html" method="POST">
+                                <a href="javascript:;" class="remove">
+                                    <i class="icon-close"></i>
+                                </a>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search...">
+                                    <span class="input-group-btn">
+                                        <a href="javascript:;" class="btn submit">
+                                            <i class="icon-magnifier"></i>
+                                        </a>
+                                    </span>
+                                </div>
+                            </form>                         
+                        </li>*/
+                  $response['menu'].='<li class="nav-item start ">
+                            <a href="#" class="nav-link nav-toggle">
+                                <i class="icon-home"></i>
+                                <span class="title">Hoteles</span>
+                                <span class="selected"></span>
+                                <span class="arrow open"></span>
+                            </a>
+                            <ul class="sub-menu">
+                                <li class="nav-item start">
+                                    <a href="<?=base_url();?>index.php/hoteles" class="nav-link ">
+                                        <i class="icon-bar-chart"></i>
+                                        <span class="title">Hoteles</span>
+                                        <span class="selected"></span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>'
+                        ; 
+
+
+		echo json_encode($response);
+	}
 	
 	///tomavariables para conectar....
 	
---
+++ assets/paginas/principal.js
@@ -0,0 +1,57 @@
+
+$.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
+
+
+function peticiones(method,url,datatype,data,funcionsuccess,funcionerror)
+{
+
+	$.ajax({
+  method: method,
+  url: url,
+  dataType: datatype,
+  data: data,
+  success: funcionsuccess,
+  error:funcionerror
+});
+}
+
+
+var mierror = function (jqXHR, textStatus, errorThrown) {
+                  if (jqXHR.status == 500) {
+                      alert('Internal error: ' + jqXHR.responseText);
+                  } else {
+                      alert('Unexpected error.');
+                  }
+              }
+
+
+
+
+peticiones('GET',base_url+'index.php/inicio/menu','JSON',$('#sinnombre').serializeObject(),function(data)
+	{
+
+$('#ulmenu').html(data.menu);
+console.log(data.menu);
+
+	},mierror);
+
+
+
+
+
+
